b8ca4bbab3e643a938a655d949d5e91e69e00de4
François Magimel <magimel.francois@gmail.com>
2022-01-04 20:49:15 +0100

    build(python): remove python 2 support

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ if os.getenv("WIN_BUID"):
 # setup
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = [
-    "pytest-runner<5.3;python_version<'3.3'",
     "pytest-runner;python_version>'3.3'",
 ] if needs_pytest else []
 setup(
@@ -46,9 +45,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Multimedia',
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Software Development :: Libraries',
