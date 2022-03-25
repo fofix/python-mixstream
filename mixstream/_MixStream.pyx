c6d21a8da3ae03011ab8cc1813f95fedae1b5881
François Magimel <magimel.francois@gmail.com>
2022-03-25 22:20:21 +0100

    feat(cython): add a compiler directive for py3
    This directive will tell to Cython that the python language level to be
    used for module compilation is python 3.

--- mixstream/_MixStream.pyx
+++ mixstream/_MixStream.pyx
@@ -18,6 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,        #
 # MA  02110-1301, USA.                                              #
 #####################################################################
+# cython: language_level=3
 
 __version__ = "1.0.1.dev0"
 
